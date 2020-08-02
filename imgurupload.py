32b45b8bcf2f77e9d19360ac865b96207e4bbcc6
deehrlic <47901184+deehrlic@users.noreply.github.com>
2020-08-01 17:38:45 -0700

    Delete imgurupload.py

--- imgurupload.py
+++
@@ -1,9 +0,0 @@
-from imgurpython import ImgurClient
-
-client_id = "9c5e85230d3df52"
-client_secret = "3d722b8f08385aef8ebb92c284457325ecb15347"
-
-client = ImgurClient(client_id,client_secret)
-
-response = client.upload_from_path("pog.png")
-print(response["link"][:-1])
